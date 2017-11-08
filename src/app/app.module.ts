e1da0ed3c0e7364e5babc3f7002dd15cac91521f
FabianSchwaiger <fabian.schwaiger@aon.at>
2017-11-08 14:46:39 +0100

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+// import { MatProgressSpinnerModule, MatProgressBarModule, MatTabsModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { StartComponent} from './start/start.component';
+import { OverviewComponent } from './overview/overview.component';
+import { FileService } from './file.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, HttpModule, AppRoutingModule ],
+  declarations: [ AppComponent, StartComponent, OverviewComponent ],     // FileDropDirective, FileSelectDirective
+  providers:    [ FileService ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
